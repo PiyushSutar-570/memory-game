efd001f38d77510ce2af14d455b40b6b2aa0bc5a
PiyushSutar-570 <piyushsutar570@gmail.com>
2025-10-08 01:54:56 +0530

    Updated memory-game: fixed Vite plugin, modified Card and Demo components

--- src/Card.jsx
+++ src/Card.jsx
@@ -4,7 +4,7 @@ const Card = ({ element }) => {
   return (
     <div className='w-[70px] h-[70px] sm:w-[110px] sm:h-[110px]'>
       <img
-        src={element.show ? element.image : 'https://icon-library.com/images/question-mark-icon-free/question-mark-icon-free-3.jpg'}
+        src={element.show ? element.image : 'https://wikiclipart.com/wp-content/uploads/2016/10/Question-mark-clip-art-question-image-4.png'}
         alt="fruit"
         className={`w-full h-full object-contain ${!element.show && 'bg-white rounded-full'}`}
       />
